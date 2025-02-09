93eac10d16ff987d2e5facccfc02e9c90142d325
mo14337 <mohitsingh14336@gmail.com>
2025-02-09 18:03:45 +0530

    Add users table

--- src/http/api.ts
+++ src/http/api.ts
@@ -4,6 +4,8 @@ import { api } from "./client";
 //Auth service
 export const login = (credetials: ICredentials) =>
   api.post("/auth/login", credetials);
-
 export const self = () => api.get("/auth/self");
 export const logout = () => api.post("/auth/logout");
+
+// users
+export const getUsers = () => api.get("/users");
--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,13 +1,65 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
+import { User } from "../../store";
+
+const columns = [
+  {
+    title: "Id",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <Link to="/users">
+          {record.firstName} {record.lastName}
+        </Link>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 
 const Users = () => {
+  const {
+    data: usersData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      return await getUsers().then((res) => res.data);
+    },
+  });
+
   return (
     <>
       <Breadcrumb
         separator={<RightOutlined />}
         items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+      />
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{error.message}</div>}
+      <Table
+        style={{ marginTop: "24px" }}
+        columns={columns}
+        dataSource={usersData}
       />
     </>
   );
